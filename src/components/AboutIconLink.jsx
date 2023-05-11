478a0be24faca760512c9ee434f18c9acf02d998
swash0 <bhandariswastika79@gmail.com>
2023-05-11 20:00:37 +0545

    first commit

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FaQuestion } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+function AboutIconLink() {
+    return (
+        <div className="about-link">
+            <Link to="/about">
+                <div className="link">
+                    <FaQuestion size={30} />
+                </div>
+            </Link>
+        </div>
+    );
+}
+
+
+export default AboutIconLink;
